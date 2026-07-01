c437a5347c27c83be7f89ed361bb221d16b34648
sj7377617 <sj7377617@gmail.com>
2026-07-01 16:15:13 +0000

    datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alret (3+3) // we are using nodejs , not  browser
+
+
+console.log(3 + 3);
+
+console.log("sakshi")
+
+let name = "sakshi"
+let age = 18
+let isLoggedIn = false
+
+//number => 2 tp power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => stanloan value
+//undefind =>
+// symbol => unique
+
+// object 
+
+console.log(typeof "sakshi");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefind);//undefind
+
+
