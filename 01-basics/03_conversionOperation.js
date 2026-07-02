3045db8f233ed3a3a39a8169f696d86cbca4797a
sj7377617 <sj7377617@gmail.com>
2026-07-02 16:32:12 +0000

    conversion & operation

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,68 @@
+// let score = 33
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// // let score = "33"
+// // console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// let score = undefined
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score = null
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score = true
+// console.log(typeof score);
+
+// let valueInNumber =  Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+let score = "sakshi"
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=> 33 
+//true => 1;
+//false => 0;
+
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true
+//0 =>  false 
+//"" => false
+//"sakshi" => true
+
+
+
+
+
+
