bac7280a422ce63713c5bfd2e5967e82b3d22b26
sj7377617 <sj7377617@gmail.com>
2026-07-05 07:47:34 +0000

    datatypes-summary

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// # Primitive :-
+
+// type 7  : String , Number , Boolean , null , undefined , Symbol , BigInt 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 34265625467687785n
+
+// Reference (Non- primitive) :-
+
+//Array , Object , Functions
+
+const heros = ["shaktiman ,'naagraj", " dogs "];
+
+let myObj = {
+    name: "sakshi",
+    age: 18,
+    
+}
+
+
+const myfunction= function(){
+   console.log("Hello world");
+   
+}
+
+console.log(typeof anotherId);
