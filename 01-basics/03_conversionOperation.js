0d1e6ab445d149067fcfca313932636f88f56417
sj7377617 <sj7377617@gmail.com>
2026-07-05 03:55:30 +0000

    operation

--- 01-basics/03_conversionOperation.js
+++ 01-basics/03_conversionOperation.js
@@ -36,11 +36,11 @@
 
 
 let score = "sakshi"
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
 //"33"=> 33 
@@ -54,12 +54,60 @@ console.log(valueInNumber);
 
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 //1 => true
 //0 =>  false 
 //"" => false
 //"sakshi" => true
+
+
+//********************** Operation **************************//
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = "sakshi"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
