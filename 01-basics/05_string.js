fca32931d9cf495cbe83399a055f2e21373d8dbf
Sakshi Jaiswal <sj7377617@gmail.com>
2026-07-09 16:45:14 +0000

    String

---
+++ 01-basics/05_string.js
@@ -0,0 +1,35 @@
+const name = "sakshi"
+const repoCount = 10
+
+console.log(name + repoCount +"value");
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+
+//const gameName = new String('hiteshhhc')
+//console.log(gameName[0]);
+//console.log(gameName._Proto__.);  synatx
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf(h));
+
+//const gameName = new String("hitesh-hc")
+//const newString = gameName.substring(0,4)
+//console.log(newString);
+
+//const anotherString  = gameName.slice(-8,4)
+//console.log(anotherString);
+
+//const newstringOne = "  hitesh  "
+//console.log(newstringOne);
+//console.log(newstringOne.trim());
+
+const url = "https://sakshi.com/sakshi%20jaiswal"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('sakshi'));
+
+const gameName = new String ("hitesh-hc");
+console.log(gameName.split('-'));
+
