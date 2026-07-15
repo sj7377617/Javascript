10e05e190cec34a3323cd8b96e1c5ab6b376e488
Sakshi Jaiswal <sj7377617@gmail.com>
2026-07-15 17:10:02 +0000

    02_object

---
+++ 02-basics/04_object.js
@@ -0,0 +1,58 @@
+const tinderUser = new Object()
+//const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regularUser = {
+    email : "sammy@gmail.com",
+    fullname :{
+        userfullname :{
+            firstname : "sakshi",
+            lastname : "jaiswal"
+        }
+    }
+}
+console.log(regularUser.fullname);
+console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = {1:"a" , 2:"b"}
+const obj2 = {3:"a" , 4:"b"}
+//const obj3 = {obj1 , obj2}
+//console.log(obj3);
+
+
+//const obj3 = Object.assign({},obj1 , obj2)  //assign operator
+//console.log(obj3);
+
+const obj3 = {...obj1 , ...obj2} // spread operator
+//console.log(obj3);
+
+
+const User =[
+    {
+        id : 1,
+        email : "s@gmail.com"
+    },
+    {
+        id : 1,
+        email : "s@gmail.com"
+    },
+    {
+        id : 1,
+        email : "s@gmail.com"
+    }
+]
+User[1].email
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
+console.log(tinderUser.hasOwnProperty("isLogged"));
