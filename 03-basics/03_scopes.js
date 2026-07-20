0e09cff0933dfb195824e3125be8dead9638e90c
Sakshi Jaiswal <sj7377617@gmail.com>
2026-07-20 16:22:26 +0000

    scopes

---
+++ 03-basics/03_scopes.js
@@ -0,0 +1,19 @@
+//let a = 10
+//const b = 20
+//var c = 30
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+if (true){
+    let a = 100
+    const b = 200
+    var c = 300
+}
+//console.log(a);
+//console.log(b);
+console.log(c);
+
+ // let and const is block scope and var is global scope
