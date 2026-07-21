87c3987fcd387005f48483e428f431fdb4991b3d
Sakshi Jaiswal <sj7377617@gmail.com>
2026-07-21 16:14:05 +0000

    scope

--- 03-basics/03_scopes.js
+++ 03-basics/03_scopes.js
@@ -8,12 +8,73 @@
 
 
 if (true){
-    let a = 100
+    //let a = 100
     const b = 200
     var c = 300
 }
 //console.log(a);
 //console.log(b);
-console.log(c);
+//console.log(c);
 
  // let and const is block scope and var is global scope
+
+let a = 10
+if (true){
+    let a = 50
+    //console.log("INNER:" , a);
+    
+}
+//console.log(a);
+
+
+
+
+
+///nested loop or closure
+
+function one(){
+    const username = "sakshi"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    //console.log(website);
+
+    two()
+
+    
+}
+
+one()
+
+if (true){
+    const username = "sakshi"
+    if (username === "sakshi"){
+        const website = " youtube"
+        console.log(username + website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(username);
+
+
+
+
+///// hoisting//
+
+console.log(addone(5));
+
+function addone(num){
+    return num + 1
+}
+
+ //console.log (addone(5))
+
+const addtwo = function(num){
+    return num + 2
+}
+console.log(addtwo(5))
